edace0206cf432e08824f4967c4872e60711db10
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-04-04 04:16:33 +0900

    ..

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -1,14 +1,16 @@
 class ConversationChannel < ApplicationCable::Channel
   def subscribed
-    @conversation = Conversation.find_by(id: params[:conversations])
+    @conversation = Conversation.find_by(id: params[:conversation])
     stream_for @conversation
+  end
+
+
+
+  def received(data)
+    ConversationChannel.broadcast_to(@conversation, {conversation: @conversation, users: @conversation.users, messages: @conversation.messages})
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-  end
-
-  def received(data)
-    ConversationChannel.broadcast_to(@conversations, {conversations: @conversations, users: @conversations.users, messages: @conversations.messages})
   end
 end
