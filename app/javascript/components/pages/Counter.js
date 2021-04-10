3fd3562231643966a522ee81b168fcec4da0d1d4
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-04-10 17:34:45 +0900

    update

--- app/javascript/components/pages/Counter.js
+++ app/javascript/components/pages/Counter.js
@@ -4,16 +4,16 @@ function Counter() {
   return (
     <div className="grid grid-cols-3 mx-20 mt-16 gap-3">
       <div className="bg-blue-400 text-white text-center py-6 rounded">
-        <p className="text-4xl">3</p>
-        <p className="">Pending Requesting</p>
+        <p className="text-4xl">03</p>
+        <p className="text-xl">Pending Requests</p>
       </div>
       <div className="bg-blue-400 text-white text-center py-6 rounded">
-        <p className="text-4xl">5</p>
-        <p>Volunteers</p>
+        <p className="text-4xl">05</p>
+        <p className="text-xl">Volunteers</p>
       </div>
       <div className="bg-blue-400 text-white text-center py-6 rounded">
         <p className="text-4xl">10</p>
-        <p>Satisfied users</p>
+        <p className="text-xl">Satisfied users</p>
       </div>
     </div>
   );
--- app/javascript/components/pages/Footer.js
+++ app/javascript/components/pages/Footer.js
@@ -1,4 +1,16 @@
 import React from "react";
-
-function Footer() {}
+// import InstagramIcon from "@material-ui/icons/Instagram"
+function Footer() {
+    return (
+        <div className="grid grid-cols-2 px-20 text-white bottom-full relative absolute bottom-0 bg-gray-800 h-36">
+            <div>
+                <h2 className="uppercase">Helping hands</h2>
+            </div>
+            <div className="">
+                <p>1001,Wall Street NY</p>
+                {/* <InstagramIcon></InstagramIcon> */}
+            </div>
+        </div>
+    )
+}
 export default Footer;
