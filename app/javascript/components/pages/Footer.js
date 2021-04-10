294cf82207ce49ff00e154e8d2d8c3563bf851fc
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-04-10 22:34:11 +0900

    fix margin footer

--- app/javascript/components/pages/Footer.js
+++ app/javascript/components/pages/Footer.js
@@ -2,16 +2,17 @@ import React from "react";
 
 // import InstagramIcon from "@material-ui/icons/Instagram"
 function Footer() {
-    return (
-        <div className="grid grid-cols-2 px-20 text-white bottom-full relative absolute bottom-0 bg-gray-800 h-36 mt-20">
-            <div>
-                <h2 className="uppercase">Helping hands</h2>
-            </div>
-            <div className="">
-                <p>1001,Wall Street NY</p>
-                {/* <InstagramIcon></InstagramIcon> */}
-            </div>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2 px-20 text-white relative bg-gray-800 h-36 mt-20">
+      {/* absolute bottom-0  bottom-full */}
+      <div>
+        <h2 className="uppercase">Helping hands</h2>
+      </div>
+      <div className="">
+        <p>1001,Wall Street NY</p>
+        {/* <InstagramIcon></InstagramIcon> */}
+      </div>
+    </div>
+  );
 }
 export default Footer;
