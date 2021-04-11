74f7c0afd08f97cc7fa370d5775a5c0706c7a999
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2021-04-11 16:35:30 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {"helping-neighbours.herokuapp.com"}
+  config.action_mailer.default_url_options = {host: "helping-neighbours.herokuapp.com"}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
